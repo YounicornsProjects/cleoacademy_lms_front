90a77a80f93f28bc68b764f728ebff12c4f20a88
jh-younicorns <162381903+jh-younicorns@users.noreply.github.com>
2025-04-08 01:27:01 +0200

    Finish restoring
    Signed-off-by: jh-younicorns <162381903+jh-younicorns@users.noreply.github.com>

--- tutorcleo/plugin.py
+++ tutorcleo/plugin.py
@@ -21,45 +21,45 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        # "WELCOME_MESSAGE": "The place for all your online learning",
-        # "PRIMARY_COLOR": "#15376D",  # Indigo
-        # "ENABLE_DARK_TOGGLE": True,
-        # # Footer links are dictionaries with a "title" and "url"
-        # # To remove all links, run:
-        # # tutor config save --set CLEO_FOOTER_NAV_LINKS=[]
-        # "FOOTER_NAV_LINKS": [
-        #     {"title": "About Us", "url": "/about"},
-        #     {"title": "Blog", "url": "/blog"},
-        #     {"title": "Donate", "url": "/donate"},
-        #     {"title": "Terms of Service", "url": "/tos"},
-        #     {"title": "Privacy Policy", "url": "/privacy"},
-        #     {"title": "Help", "url": "/help"},
-        #     {"title": "Contact Us", "url": "/contact"},
-        # ],
+        "WELCOME_MESSAGE": "The place for all your online learning",
+        "PRIMARY_COLOR": "#15376D",  # Indigo
+        "ENABLE_DARK_TOGGLE": True,
+        # Footer links are dictionaries with a "title" and "url"
+        # To remove all links, run:
+        # tutor config save --set CLEO_FOOTER_NAV_LINKS=[]
+        "FOOTER_NAV_LINKS": [
+            {"title": "About Us", "url": "/about"},
+            {"title": "Blog", "url": "/blog"},
+            {"title": "Donate", "url": "/donate"},
+            {"title": "Terms of Service", "url": "/tos"},
+            {"title": "Privacy Policy", "url": "/privacy"},
+            {"title": "Help", "url": "/help"},
+            {"title": "Contact Us", "url": "/contact"},
+        ],
     },
     "unique": {},
     "overrides": {},
 }
 
-# # Theme templates
-# hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
-#     str(importlib_resources.files("tutorcleo") / "templates")
-# )
-# # This is where the theme is rendered in the openedx build directory
-# hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
-#     [
-#         ("cleo", "build/openedx/themes"),
-#         ("cleo/env.config.jsx", "plugins/mfe/build/mfe"),
-#     ],
-# )
+# Theme templates
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    str(importlib_resources.files("tutorcleo") / "templates")
+)
+# This is where the theme is rendered in the openedx build directory
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("cleo", "build/openedx/themes"),
+        ("cleo/env.config.jsx", "plugins/mfe/build/mfe"),
+    ],
+)
 
-# # Force the rendering of scss files, even though they are included in a "partials" directory
-# hooks.Filters.ENV_PATTERNS_INCLUDE.add_items(
-#     [
-#         r"cleo/lms/static/sass/partials/lms/theme/",
-#         r"cleo/cms/static/sass/partials/cms/theme/",
-#     ]
-# )
+# Force the rendering of scss files, even though they are included in a "partials" directory
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_items(
+    [
+        r"cleo/lms/static/sass/partials/lms/theme/",
+        r"cleo/cms/static/sass/partials/cms/theme/",
+    ]
+)
 
 
 # init script: set theme automatically
@@ -104,118 +104,118 @@ hooks.Filters.CONFIG_UNIQUE.add_items(
 hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
 
-# #  MFEs that are styled using Indigo
-# cleo_styled_mfes = [
-#     "learning",
-#     "learner-dashboard",
-#     "profile",
-#     "account",
-#     "discussions",
-# ]
+#  MFEs that are styled using Indigo
+cleo_styled_mfes = [
+    "learning",
+    "learner-dashboard",
+    "profile",
+    "account",
+    "discussions",
+]
 
-# hooks.Filters.ENV_PATCHES.add_items(
-#     [
-#         (
-#             f"mfe-dockerfile-post-npm-install-{mfe}",
-#             """
+hooks.Filters.ENV_PATCHES.add_items(
+    [
+        (
+            f"mfe-dockerfile-post-npm-install-{mfe}",
+            """
            
-# RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
-# RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
-# RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
+RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
+RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
+RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
 
-# """,
-#         )
-#         for mfe in cleo_styled_mfes
-#     ]
-# )
-
-
-# hooks.Filters.ENV_PATCHES.add_item(
-#     (
-#         "mfe-dockerfile-post-npm-install-authn",
-#         "RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'",
-#     )
-# )
-
-# # Include js file in lms main.html, main_django.html, and certificate.html
-
-# hooks.Filters.ENV_PATCHES.add_items(
-#     [
-#         # for production
-#         (
-#             "openedx-common-assets-settings",
-#             """
-# javascript_files = ['base_application', 'application', 'certificates_wv']
-# dark_theme_filepath = ['cleo/js/dark-theme.js']
-
-# for filename in javascript_files:
-#     if filename in PIPELINE['JAVASCRIPT']:
-#         PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
-# """,
-#         ),
-#         # for development
-#         (
-#             "openedx-lms-development-settings",
-#             """
-# javascript_files = ['base_application', 'application', 'certificates_wv']
-# dark_theme_filepath = ['cleo/js/dark-theme.js']
-
-# for filename in javascript_files:
-#     if filename in PIPELINE['JAVASCRIPT']:
-#         PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
-
-# MFE_CONFIG['CLEO_ENABLE_DARK_TOGGLE'] = {{ CLEO_ENABLE_DARK_TOGGLE }}
-# """,
-#         ),
-#         (
-#             "openedx-lms-production-settings",
-#             """
-# MFE_CONFIG['CLEO_ENABLE_DARK_TOGGLE'] = {{ CLEO_ENABLE_DARK_TOGGLE }}
-# """,
-#         ),
-#     ]
-# )
+""",
+        )
+        for mfe in cleo_styled_mfes
+    ]
+)
 
 
-# # Apply patches from tutor-indigo
-# for path in glob(
-#     os.path.join(
-#         str(importlib_resources.files("tutorcleo") / "patches"),
-#         "*",
-#     )
-# ):
-#     with open(path, encoding="utf-8") as patch_file:
-#         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-dockerfile-post-npm-install-authn",
+        "RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'",
+    )
+)
+
+# Include js file in lms main.html, main_django.html, and certificate.html
+
+hooks.Filters.ENV_PATCHES.add_items(
+    [
+        # for production
+        (
+            "openedx-common-assets-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['cleo/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+""",
+        ),
+        # for development
+        (
+            "openedx-lms-development-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['cleo/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+
+MFE_CONFIG['CLEO_ENABLE_DARK_TOGGLE'] = {{ CLEO_ENABLE_DARK_TOGGLE }}
+""",
+        ),
+        (
+            "openedx-lms-production-settings",
+            """
+MFE_CONFIG['CLEO_ENABLE_DARK_TOGGLE'] = {{ CLEO_ENABLE_DARK_TOGGLE }}
+""",
+        ),
+    ]
+)
 
 
-# for mfe in cleo_styled_mfes:
-#     PLUGIN_SLOTS.add_item(
-#         (
-#             mfe,
-#             "footer_slot",
-#             """ 
-#             {
-#                 op: PLUGIN_OPERATIONS.Hide,
-#                 widgetId: 'default_contents',
-#             },
-#             {
-#                 op: PLUGIN_OPERATIONS.Insert,
-#                 widget: {
-#                     id: 'default_contents',
-#                     type: DIRECT_PLUGIN,
-#                     priority: 1,
-#                     RenderWidget: <IndigoFooter />,
-#                 },
-#             },
-#             {
-#                 op: PLUGIN_OPERATIONS.Insert,
-#                 widget: {
-#                     id: 'read_theme_cookie',
-#                     type: DIRECT_PLUGIN,
-#                     priority: 2,
-#                     RenderWidget: AddDarkTheme,
-#                 },
-#             },
-#   """,
-#         ),
-#     )
+# Apply patches from tutor-indigo
+for path in glob(
+    os.path.join(
+        str(importlib_resources.files("tutorcleo") / "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+for mfe in cleo_styled_mfes:
+    PLUGIN_SLOTS.add_item(
+        (
+            mfe,
+            "footer_slot",
+            """ 
+            {
+                op: PLUGIN_OPERATIONS.Hide,
+                widgetId: 'default_contents',
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'default_contents',
+                    type: DIRECT_PLUGIN,
+                    priority: 1,
+                    RenderWidget: <IndigoFooter />,
+                },
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'read_theme_cookie',
+                    type: DIRECT_PLUGIN,
+                    priority: 2,
+                    RenderWidget: AddDarkTheme,
+                },
+            },
+  """,
+        ),
+    )
